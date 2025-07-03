fb6d40f35df03873abaac7426808393356cbf3e8
rafaelgoncalves1994 <rafaelsergio1994@gmail.com>
2025-07-03 19:28:42 -0300

    segurança de api externa

--- backend/weather.py
+++ backend/weather.py
@@ -1,23 +1,30 @@
 from flask import Blueprint, request, jsonify
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Carrega variáveis do .env
 
 weather_bp = Blueprint('weather', __name__)
 
 @weather_bp.route('/weather', methods=['GET'])
 def get_weather():
     city = request.args.get('city', default='Recife', type=str)
-    api_key = '18e3ecb76303610fd0cf4e74e415ac2f'  #chave da OpenWeatherMap
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=pt_br'
+    api_key = os.getenv('OPENWEATHER_API_KEY')
 
+    if not api_key:
+        return jsonify({'error': 'Chave da API não configurada'}), 500
+
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=pt_br'
     response = requests.get(url)
+
     if response.status_code == 200:
         data = response.json()
-        weather_info = {
+        return jsonify({
             'cidade': data['name'],
             'temperatura': round(data['main']['temp']),
             'descricao': data['weather'][0]['description'],
             'icone': data['weather'][0]['icon'],
-        }
-        return jsonify(weather_info)
+        })
     else:
         return jsonify({'error': 'Cidade não encontrada'}), 404
